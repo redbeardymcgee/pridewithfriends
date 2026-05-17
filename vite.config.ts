39f7a2ed7652bb266d33ce6bd8f22cf446b4284b
redbeardymcgee <redbeardymcgee@proton.me>
2026-05-17 16:00:03 -0500

    enable react server component support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
+import rsc from "@vitejs/plugin-rsc"
 import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
@@ -12,7 +13,12 @@ const config = defineConfig({
     devtools(),
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      rsc: {
+        enabled: true,
+      },
+    }),
+    rsc(),
     viteReact(),
     nitro(),
     svgr({
