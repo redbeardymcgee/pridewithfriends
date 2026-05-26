f41088eea289bc9b52e34fa1320837b615bea537
redbeardymcgee <redbeardymcgee@proton.me>
2026-05-25 23:30:55 -0500

    rewrite campaign setup instructions

--- src/routes/docs/campaign-setup.tsx
+++ src/routes/docs/campaign-setup.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { Image } from "@unpic/react"
 import {
   DocsH2,
   ExampleText,
+  PrideText,
   TutorialText,
-  WarningText,
 } from "#/components/typography"
+import createCampaign from "@/assets/images/docs/create-campaign.png"
+import reviewPublish from "@/assets/images/docs/review-publish.png"
 
 export const Route = createFileRoute("/docs/campaign-setup")({
   component: CampaignSetup,
@@ -14,64 +17,91 @@ function CampaignSetup() {
   return (
     <div className="mx-auto max-w-prose">
       <DocsH2>Campaign Setup</DocsH2>
-      <p>
-        If you are a streamer and want to have your own individual donation
-        tracker, overlay, incentives and milestones, follow these instructions.
-      </p>
       <br />
-      <ol className="list-decimal pl-10">
+      <p>
+        Now that you are a member of the team, you will be able to automatically
+        add your own fundraiser to the overall campaign!
+      </p>
+      <ol className="list-disc pl-10">
+        <li>Ensure you are logged into your Tiltify account, then click </li>
+        <a
+          href="https://start.tiltify.com?supportingFactId=34a3bb09-ed1e-4930-9f0b-348723ad829d"
+          rel="noreferrer"
+          target="_blank"
+        >
+          this link
+        </a>
+        .
         <li>
-          Click {/* FIXME: Update link for new campaign */}
-          <a
-            href="https://tilt.fyi/mqTk1CP3dQ"
-            rel="noreferrer"
-            target="_blank"
-          >
-            here
-          </a>{" "}
-          (<WarningText>WARNING:</WarningText>
-          <em> This is not updated yet</em>)
+          Click <TutorialText>Skip with Quick Start</TutorialText> to choose how
+          you will share the campaign with donors.
         </li>
         <li>
-          Click <TutorialText>Support this campaign</TutorialText>
+          Select <TutorialText>Sharing on Social</TutorialText> and{" "}
+          <TutorialText>Live Streaming</TutorialText>.
         </li>
         <li>
-          Fill in the <TutorialText>Campaign Name</TutorialText> (e.g{" "}
-          <ExampleText>Pride With Friends 2026 [YOURNAME]</ExampleText>)
+          <TutorialText>Confirm</TutorialText> your choice
         </li>
         <li>
-          Add a description (e.g.{" "}
-          <ExampleText>
-            [YOURNAME] climbs spires and slays hearts with friends to support
-            the Trevor Project!
-          </ExampleText>
-          )
-        </li>
-        <li>Link your Twitch account name or skip</li>
-        <li>
-          {/* TODO: Create PWF explainer/invite video? */}
-          Skip the Youtube video
-        </li>
-        <li>Skip Image</li>
-        <li>
-          Set your own campaign goal! $100 is the minimum on Tiltify, but you
-          are under zero pressure to reach it at all. Every penny counts towards
-          supporting a good cause, and combines with the whole campaign!
-        </li>
-        <li>
-          {" "}
-          Click <TutorialText>Create Campaign</TutorialText>
+          Select <TutorialText>No</TutorialText> when it suggests to allow other
+          creators to support your fundraising effort. They will already be
+          following these same steps to support the{" "}
+          <PrideText>Pride With Friends</PrideText> campaign. It should look
+          like the below image.
         </li>
       </ol>
       <br />
+      <Image
+        alt="Create campaign wizard demonstrating the correct selections"
+        className="place-self-center"
+        height={500}
+        layout="constrained"
+        src={createCampaign}
+        width={400}
+      />
+      <br />
       <p>
-        You will now be at the Campaign Dashboard for Pride With Friends. This
-        is where you edit any additional information. You do not have to change
-        or add anything here, but it is the place where you configure donation
-        incentives and milestones. After setting those up, you click the blue
-        publish button and start adding the campaign and donate urls to your
-        stream panels and bot commands! You will also find the overlays section,
-        for adding your own donation tracker and alerts to your stream.
+        After confirming, you will be offered an opportunity to edit a couple
+        more details.
+      </p>
+      <br />
+      <ol className="list-disc pl-10">
+        <li>
+          Please edit the <TutorialText>Campaign name</TutorialText> to clarify
+          that it is a <PrideText>Pride With Friends</PrideText> fundraiser.
+          Most other participants will name it
+          <ExampleText>Pride With Friends 2026 [YOURNAME]</ExampleText>).
+        </li>
+        <li>
+          Feel free to add your own description in{" "}
+          <TutorialText>About your campaign</TutorialText>, but ensure that the
+          Mermaids charity we are supporting is mentioned!
+        </li>
+        <li>
+          Set the <TutorialText>Goal</TutorialText> to any value you feel is
+          both comfortable and achievable.
+        </li>
+        <li>
+          You may now <TutorialText>Save & publish</TutorialText> to officially
+          launch your <PrideText>Pride With Friends</PrideText> fundraiser!
+        </li>
+      </ol>
+      <br />
+      <Image
+        alt="Final details editor before publishing"
+        className="place-self-center"
+        layout="fullWidth"
+        // FIXME: This image needs to change the campaign name for demonstration
+        src={reviewPublish}
+      />
+      <br />
+      <p>
+        You will now be at the Campaign Dashboard for{" "}
+        <PrideText>Pride With Friends</PrideText>. This is where you may edit
+        additional details of the campaign to increase the chance of reaching
+        your donation goal.
+        {/* TODO: Update this with a link to next steps, such as editing incentives or sharing campaign/dono links */}
       </p>
     </div>
   )
