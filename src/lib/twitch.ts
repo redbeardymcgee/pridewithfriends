a6647fd0682f59c766e0d3fad89e5842246bbc79
redbeardymcgee <redbeardymcgee@proton.me>
2026-06-02 15:52:46 -0500

    extract twitch client
    This is intended to ensure that only 1 instance of the client is used
    across the full app. It's probably not that important.

--- src/lib/twitch.ts
+++ src/lib/twitch.ts
@@ -1,21 +1,19 @@
 import { ApiClient } from "@twurple/api"
 import { AppTokenAuthProvider } from "@twurple/auth"
 
-function getTwitchClient(): ApiClient {
-  const clientId = process.env.TWITCH_CLIENT_ID
-  const clientSecret = process.env.TWITCH_CLIENT_SECRET
+const clientId = process.env.TWITCH_CLIENT_ID
+const clientSecret = process.env.TWITCH_CLIENT_SECRET
 
-  if (!clientId || !clientSecret) {
-    throw new Error(
-      "Missing Twitch API credentials. Check the .env, .env.development, .env.local and .env.production files.",
-    )
-  }
-
-  const authProvider = new AppTokenAuthProvider(clientId, clientSecret)
-  return new ApiClient({ authProvider })
+if (!clientId || !clientSecret) {
+  throw new Error(
+    "Missing Twitch API credentials. Check the .env, .env.development, .env.local and .env.production files.",
+  )
 }
 
+const twitchClient = new ApiClient({
+  authProvider: new AppTokenAuthProvider(clientId, clientSecret),
+})
+
 export async function getTwitchUsersByNames(names: string[]) {
-  const apiClient = getTwitchClient()
-  return await apiClient.users.getUsersByNames(names)
+  return await twitchClient.users.getUsersByNames(names)
 }
